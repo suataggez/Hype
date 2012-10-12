110256cb760091126225828e94cfde3da56c28b3
root <root@suat.(none)>
2012-10-12 11:48:29 +0300

    bookmarkk

---
+++ bookmarks.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<title>Hype Bookmarks</title>
+</head>
+<body>
+<? 
+include('menu.php');
+?>
+
+</body>
+</html>
